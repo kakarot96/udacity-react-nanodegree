216d5c7ef39d5855fa1b17cf178ccc713d621fa9
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2020-03-29 04:17:07 +0530

    Lesson 3 Exercise workspace copied

---
+++ ContactsApp/src/ListContacts.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const ListContacts = (props) => {
+    console.log(props)
+
+    return (
+       <ol>
+          {props.contacts.map((contact,index)=>(
+            <li key={contact.id} className='contact-list-item'>
+            <div
+              className='contact-avatar'
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            ></div>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              
+              <p>{contact.handle}</p>
+            </div>
+            <button className='contact-remove'>
+              Remove
+            </button>
+            </li>
+          ))} 
+       </ol>
+    );
+};
+
+export default ListContacts;
